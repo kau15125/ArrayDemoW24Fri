c240a72360266ce84a35f4f97598c63cc7209306
Kiranpreet Kaur <kau15125@sheridancollege.ca>
2024-01-26 16:49:40 -0500

    added a class

---
+++ src/studentprint/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentprint;
+
+/**
+ *
+ * @author kiran
+ */
+public class PartTimeStudent extend Student{
+    private int NumbrerofCourses;
+
+    public PartTimeStudent(int NumbrerofCourses) {
+        this.NumbrerofCourses = NumbrerofCourses;
+    }
+
+    public int getNumbrerofCourses() {
+        return NumbrerofCourses;
+    }
+
+    public void setNumbrerofCourses(int NumbrerofCourses) {
+        this.NumbrerofCourses = NumbrerofCourses;
+    }
+    
+    
+    
+}
